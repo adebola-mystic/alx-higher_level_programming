d7a31528864b6ea9f05a8e50b286e4b08dcaac02
adebola-mystic <adebolasonofadam@gmail.com>
2023-11-28 10:18:30 -0500

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Oni Adebola
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author -Oni Adebola
+def pow(a, b):
+    return (a ** b)
