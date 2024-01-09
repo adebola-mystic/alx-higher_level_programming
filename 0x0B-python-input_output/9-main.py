531616172b8a8ae78127e925e1d8247789026439
adebola-mystic <adebolasonofadam@gmail.com>
2024-01-09 18:05:47 -0500

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/9-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+Student = __import__('9-student').Student
+
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
+
+guillaume@ubuntu:~/0x0B$ ./9-main.py 
+<class 'dict'>
+John
+<class 'str'>
+23
+<class 'int'>
+<class 'dict'>
+Bob
+<class 'str'>
+27
+<class 'int'>
